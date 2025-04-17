9d59d381250ca372762f6557a0e82bf6e8b741de
Ahmed-Mohey <you@example.com>
2025-04-17 17:15:50 +0200

    oV250417_0515

--- Apps/aProject/urls.py
+++ Apps/aProject/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('projects/', views.project_list, name='projects_list'),
+    path('projects/', views.project_list, name='project_lists'),
     path('editprojects/<int:project_id>/', views.edit_project, name='edit_projects'),
     path('deleteprojects/<int:project_id>/', views.delete_project, name='delete_projects'),    
     path('get_machines/<int:project_id>/', views.get_machines, name='get_machiness'),    
